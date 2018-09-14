cf41f361db6c4df72e1e265e9da0640e8b021026
xuanxuan <544493924@qq.com>
2018-09-14 14:40:27 +0800

    修改了必要的越是

--- MarkLearn/apps/TraverMsg/admin.py
+++ MarkLearn/apps/TraverMsg/admin.py
@@ -1,6 +1,5 @@
 import xadmin
 from .models import CityMsg
-from .models import TraverMsg
 from .models import ScenicMsg
 from .models import ProvinceMsg
 
@@ -8,44 +7,33 @@ from .models import ProvinceMsg
 # Register your models here.
 class CityMsgAdmin(object):
     # 显示的列
-    list_display = ['id', 'name', 'city_content', 'img_url', 'province']
+    list_display = ['id', 'name', 'province_name','img_url']
     # 搜索的字段
     search_fields = ['id', 'name']
     # 过滤
-    list_filter = ['id', 'name', 'city_content', 'img_url', 'province']
+    list_filter = ['id', 'name', 'province_name','img_url']
 
-
-class TraverMsgAdmin(object):
-    # 显示的列
-    list_display = ['id', 'name', 'user_id', 'start_city', 'end_city',
-                    'traverdays', 'adult_num', 'child_num', 'traver_type']
-    # 搜索的字段
-    search_fields = ['name', 'traver_type']
-    # 过滤
-    list_filter = ['id', 'name', 'user_id', 'start_city', 'end_city',
-                   'traverdays', 'adult_num', 'child_num', 'traver_type']
 
 
 class ScenicMsgAdmin(object):
     # 显示的列
-    list_display = ['id', 'name', 'city_name']
+    list_display = ['id', 'name', 'city_name','img_url']
     # 搜索的字段
-    search_fields = ['name','city_name']
+    search_fields = ['name','city_name','img_url']
     # 过滤
-    list_filter = ['id', 'name','city_name']
+    list_filter = ['id', 'name','city_name','img_url']
 
 
 class ProvinceMsgAdmin(object):
     # 显示的列
-    list_display = ['id', 'name', 'province_content', 'img_url']
+    list_display = ['id', 'name','img_url']
     # 搜索的字段
-    search_fields = ['name']
+    search_fields = ['name','img_url']
     # 过滤
     list_filter = ['id', 'name', 'img_url']
 
 
 # 使用xadmin进行数据字段的注册
 xadmin.site.register(CityMsg, CityMsgAdmin)
-xadmin.site.register(TraverMsg, TraverMsgAdmin)
 xadmin.site.register(ScenicMsg, ScenicMsgAdmin)
 xadmin.site.register(ProvinceMsg, ProvinceMsgAdmin)
--- MarkLearn/apps/TraverMsg/models.py
+++ MarkLearn/apps/TraverMsg/models.py
@@ -1,7 +1,7 @@
 # Create your models here.
 # 行程信息模块的数据
 from django.db import models
-from Account.models import UserProfile
+
 
 """
 # 1.省份信息
@@ -10,9 +10,8 @@ from Account.models import UserProfile
 
 class ProvinceMsg(models.Model):
     id = models.CharField('省份id',max_length=20, primary_key=True)
-    name = models.CharField('省份名称', max_length=20)
-    province_content = models.CharField('省份简介', max_length=500, null=True, blank=True)
-    img_url = models.CharField('图片url', max_length=100, null=True, blank=True)
+    name = models.CharField('省份名称', max_length=20,unique=True)
+    img_url = models.CharField('图片url',max_length=100,null = True,blank=True)
 
     class Meta:
         verbose_name = '省份信息'
@@ -22,6 +21,7 @@ class ProvinceMsg(models.Model):
         return self.name
 
 
+
 """
 # 2.城市信息
 """
@@ -29,10 +29,9 @@ class ProvinceMsg(models.Model):
 
 class CityMsg(models.Model):
     id = models.CharField('城市id', max_length=20, primary_key=True)
-    name = models.CharField('城市名称', max_length=20)
-    city_content = models.CharField('城市简介', max_length=500, null=True, blank=True)
+    name = models.CharField('城市名称', max_length=20,unique=True)
     img_url = models.CharField('首页图片url', max_length=100, null=True, blank=True)
-    province = models.ForeignKey(ProvinceMsg, verbose_name='所属省份', on_delete=models.CASCADE)
+    province_name = models.CharField('所属省份',max_length=100,null=True,blank=True)
 
     class Meta:
         verbose_name = '城市信息'
@@ -41,45 +40,16 @@ class CityMsg(models.Model):
     def __str__(self):
         return self.name
 
-
 """
-# 3.行程信息
-"""
-
-
-class TraverMsg(models.Model):
-    # 创建旅游类型
-    type_choices = (
-        ('single', '自驾游'),
-        ('together', '跟团游')
-    )
-    id = models.CharField('行程id', max_length=20, primary_key=True)
-    name = models.CharField('行程名称', max_length=20)
-    user_id = models.ForeignKey(UserProfile, verbose_name='所属用户', on_delete=models.CASCADE)
-    start_city = models.ForeignKey(CityMsg, verbose_name='开始城市', on_delete=models.CASCADE, related_name='start_city_id')
-    end_city = models.ForeignKey(CityMsg, verbose_name='目标城市', on_delete=models.CASCADE, related_name='end_city_id')
-    traverdays = models.CharField('行程天数', max_length=3)
-    adult_num = models.CharField('成人数量', max_length=3)
-    child_num = models.CharField('儿童数量', max_length=3)
-    traver_type = models.CharField('旅游类型', max_length=10, choices=type_choices, default='single')
-
-    class Meta:
-        verbose_name = '行程信息'
-        verbose_name_plural = verbose_name
-
-    def __str__(self):
-        return self.name  # 返回基类的用户名
-
-
-"""
-# 4.景点信息
+# 3.景点信息
 """
 
 
 class ScenicMsg(models.Model):
     id = models.CharField('景点id', max_length=20, primary_key=True)
     name = models.CharField('景点名称', max_length=20)
-    city_name = models.CharField('所在城市',max_length=100,null=True,blank=True)
+    img_url = models.CharField('图片url',max_length=100,null=True,blank=True)
+    city_name = models.CharField('所属城市',max_length=100,null=True,blank=True)
 
     class Meta:
         verbose_name = "景点信息"
